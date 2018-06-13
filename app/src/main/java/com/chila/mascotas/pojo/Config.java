efb1c8172c1cb19d3e87e2cfd9ea13aeaf0adeb8
Martin Gilabert <31170204+chilagranate@users.noreply.github.com>
2018-06-12 23:30:36 -0300

    Update Config.java

--- app/src/main/java/com/chila/mascotas/pojo/Config.java
+++ app/src/main/java/com/chila/mascotas/pojo/Config.java
@@ -1,6 +1,6 @@
 package com.chila.mascotas.pojo;
 
 public class Config {
-    public static final String EMAIL ="chila.granate@gmail.com";
-    public static final String PASSWORD ="2603631as";
+    public static final String EMAIL ="";
+    public static final String PASSWORD ="";
 }
